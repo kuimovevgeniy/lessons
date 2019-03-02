55549eda5d617092274da38193585de386adaaba
kuimovevgeniy <kuimovevgeniy@mail.ru>
2019-03-02 13:57:24 +0300

    создан массив

--- lesson 4/cart/cart.js
+++ lesson 4/cart/cart.js
@@ -1,6 +1,21 @@
+// var arr = [
+//     ['Трусы', 100, 1],
+//     ['Носки', 20, 4]
+// ]
+
+
+
 var arr = [
-    ['Трусы', 100, 1],
-    ['Носки', 20, 4]
+    {
+        name: 'Трусы',
+        price: 100,
+        quantity: 1,
+    },
+    {
+        name: 'Носки',
+        price: 100,
+        quantity: 1,
+    }
 ]
 
 //sum = arr[0][2] * arr[0][1] + arr[1][2] * arr[1][1]
@@ -8,7 +23,7 @@ var arr = [
 function getTotal(arr) {
     var total = 0;
     for(var i = 0; i < arr.length; i++) {
-        total = total + arr[i][1] * arr[i][2];
+        total = total + arr[i].price * arr[i].quantity;
     }
     return total;
 }
